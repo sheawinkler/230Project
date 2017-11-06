9754e3b48ac24881ff14b1b804286ab56555e55e
sheawinkler <sheawinkler@gmail.com>
2017-11-06 17:47:31 -0600

    Update MuxC.vhdl

--- Part3/MuxC.vhdl
+++ Part3/MuxC.vhdl
@@ -4,9 +4,9 @@ use ieee.std_logic_1164.all;
 entity MuxC is
 	port(
 		IR26,IR21			:	in std_logic_vector(3 downto 0);
-		constant LINK		:	std_logic_vector(3 downto 0):= "1111" ;
+		constant LINK			:	std_logic_vector(3 downto 0):= "1111" ;
 		c_Select			:	in std_logic_vector(1 downto 0);
-		addressC			:	out std_logic_vector(3 downto 0)
+		regRD				:	out std_logic_vector(3 downto 0)
 	);
 	
 end MuxC;
@@ -15,7 +15,7 @@ architecture arc_muxC of MuxC is
 
 begin
 	with c_Select select
-		addressC <= IR26 when "00",
+		regRD <= IR26 when "00",
 		IR21 when "01",
 		LINK when "10"
 end arc_muxC
