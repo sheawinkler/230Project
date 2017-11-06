04075f7094ff8b8c4fbf503e92bb88da1cb29e99
sheawinkler <sheawinkler@gmail.com>
2017-11-05 18:00:35 -0600

    Add files via upload
    MuxMA is not completed but should have the proper inputs and outputs sans the select bit(s). The control unit I made some updates to in order to incorporate branch instruction's conditional arguments. I'm not sure if it is correct but I included it in Stage 3 for now.

---
+++ Part3/MuxMA.vhdl
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity MuxMA is
+	port(
+		IAG			:	in (23 downto 0);
+		RegRZ		:	in (15 downto 0);
+		--sel			:	in (1 downto 0); **have no idea what select bit is**
+		memAddr		:	out(9 downto 0)
+	);
+	
+end MuxMA;
+
+architecture arc_MA of MuxMA is
+
+begin
+
+end arc_MA;
+
