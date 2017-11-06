5a107b6de42a1dc190957d08c4c9c1567702a39d
sheawinkler <sheawinkler@gmail.com>
2017-11-06 16:45:10 -0600

    Update MuxMA.vhdl

--- Part3/MuxMA.vhdl
+++ Part3/MuxMA.vhdl
@@ -15,5 +15,9 @@ architecture arc_MA of MuxMA is
 
 begin
 --architecture will have to use a buffer to reduce 16 bits to 10 bit output
+	with MA_Select select
+		memAddr <= RegRZ(9 downto 0) when '0',
+		IAG(9 downto 0) when '1',
+		"0000000000" when others;
 end arc_MA;
 
